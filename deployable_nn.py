5e646394b53f4e0da88f6df4ba867c19443e5009
jwaldroop <72318411+jwaldroop@users.noreply.github.com>
2021-04-26 18:11:03 -0600

    updated deployable nn version

--- deployable_nn.py
+++ deployable_nn.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
-from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -56,7 +56,6 @@ def data_time():
     #set X and y
     y = reader.iloc[:,-1]
     X = reader.iloc[:,0:111]
-    X = tf.keras.utils.normalize(X, axis=-1, order=2)
     return X, y
 
 print('X and y are set.')
@@ -69,6 +68,9 @@ def train_test():
 print('train_test_split completed')
 
 tf.keras.backend.clear_session()
+print('TensorFlow Backend Session Cleared')
+
+print("For a more detailed output, change verbosity to 'verbose = 1'")
 
 
 def phish_nn():
@@ -77,53 +79,68 @@ def phish_nn():
     train_X, val_X, train_y, val_y = train_test()
     model.add(tf.keras.layers.InputLayer(input_shape=[111]))
     model.add(tf.keras.layers.Dense(units=64, activation='relu'))
+    model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.Dense(units=64, activation='relu'))
+    model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.Dense(units=50, activation='relu'))
+    model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.Dropout(0.20))
     model.add(tf.keras.layers.Dense(units=32, activation='relu'))
+    model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.Dense(units=32, activation='relu'))
+    model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.Dense(units=16, activation='relu'))
+    model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.Dropout(0.40))
     model.add(tf.keras.layers.Dense(units=16, activation='relu'))
+    model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.Dropout(0.40))
     model.add(tf.keras.layers.Dense(units=111, activation='relu'))
+    model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    earlystopping = tf.keras.callbacks.EarlyStopping(monitor = 'val_accuracy', mode = 'max', patience = 15, restore_best_weights = True)
+    earlystopping = tf.keras.callbacks.EarlyStopping(monitor = 'val_accuracy', mode = 'max', patience = 25, restore_best_weights = True)
     print('Starting Model Fit')
-    history = model.fit(train_X, train_y, validation_split=0.30, batch_size= 264, epochs=200, callbacks = [earlystopping], workers=8, verbose=0)
-    predictions = model.predict(val_X)
+    history = model.fit(train_X, train_y, validation_split=0.30, batch_size=1500, epochs=250,
+                        callbacks = [earlystopping], workers=8, verbose=0)
+    predictions = (model.predict(val_X) > 0.5).astype('int32')
     val_scores = model.evaluate(val_X, val_y) #give the model the ability to evaluate like was done above
     #saved_model = model.save(filepath='./deploy-nn/saved_deployable_nn/', include_optimizer=True, overwrite=True)
     train_scores = model.evaluate(train_X, train_y) #give the model the ability to evaluate like was done above
     print('Training Binary Cross Entropy: \n', round(((train_scores[0])*100),2), '%')
-    print('Validation Binary Crossentropy: \n', round(((val_scores[0])*100),2), '%')
-    print('Validation Accuracy: \n', round(((val_scores[1])*100),2), '%')
     print('Training Accuracy: \n', round(((train_scores[1])*100),2), '%')
-    print('Prediction probabilities: \n', predictions)
+    print('Validation Binary Cross Entropy: \n', round(((val_scores[0])*100),2), '%')
+    print('Validation Accuracy: \n', round(((val_scores[1])*100),2), '%')
+    print('Predictions: \n', predictions)
+    cm = confusion_matrix(val_y, predictions)
+    print('Confusion Matrix: \n', cm)
     return model
 
 phish_nn()
 
+print("For a more detailed output, change verbosity to 'verbose = 1'")
+
 def cross_val_nn():
     print('Cross validation initializing')
     X, y = data_time()
-    mod = KerasClassifier(build_fn = phish_nn, epochs = 15, batch_size = 500, verbose=0)
+    mod = KerasClassifier(build_fn = phish_nn, epochs = 15, batch_size = 1500, verbose=0)
     num_folds = 5
     kfold = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=808)
     print('Starting 5-fold cross validation of model')
-    cv_results = cross_val_score(mod, X, y, cv=kfold, n_jobs=-1)
+    cv_results = cross_val_score(mod, X, y, cv=kfold)
+    print('Starting 5-fold cross-validation predictions')
+    cv_preds = cross_val_predict(mod, X, y, cv=num_folds, verbose=0, method='predict')
     print('The average cross-validation accuracy is: ', round(cv_results.mean(), 4)*100, '%')
     print('The 5-fold cross validation accuracy results are: \n', cv_results)
-    print('Starting 5-fold cross-validation predictions')
-    cv_preds = cross_val_predict(mod, X, y, cv=num_folds, verbose=0, method='predict', n_jobs=-1)
+    acc = accuracy_score(y, cv_preds)
     cm = confusion_matrix(y, cv_preds)
     print('Confusion Matrix \n', cm)
+    print('Accuracy Score: \n', acc)
     f1s = f1_score(y, cv_preds)
     print('The F1 score for the cross validated model is: \n', f1s)
     precis = precision_score(y, cv_preds, average='binary')
@@ -133,10 +150,11 @@ def cross_val_nn():
     return cm
 
 cm = cross_val_nn()
-holup = sns.heatmap(cm, annot=True, fmt='d', linewidths=0.5,
+holup2 = sns.heatmap(cm, annot=True, fmt='d', linewidths=0.5,
             xticklabels=True, yticklabels=True)
-#holup.set_xticklabels(labels=['Benign', 'Malicious'])
-#holup.set_yticklabels(labels=['Malicious', 'Benign'])
 plt.xlabel('Actual')
 plt.ylabel('Predicted')
 plt.show()
+
+
+print('Script Finished. Thank you :)')
